9b5ad7e6ab63a17ef7e29cbe6e0eddd0bae42959
Oskar Hartbrich <oskar@hartbrich.net>
2024-09-27 14:22:01 -0400

    Add files via upload

--- pfrich_contour.py
+++ pfrich_contour.py
@@ -201,10 +201,14 @@ def theta_from_xy(x, y):
 epic_B = 1.7 #epic B solenoid field in T
 
 #pfRICH geometry:
-rich_radius_min = 0.095 #m
-rich_radius_max = 0.659 #m
-
 rich_z = -1.725 #m
+
+rich_radius_min = rich_z * np.tan(eta_to_theta(-3.5)) #was 0.095 #m
+rich_radius_max = rich_z * np.tan(eta_to_theta(-1.5)) #was 0.659 #m
+
+print('radii:', rich_radius_min, rich_radius_max)
+
+
 
 rich_theta_min = theta_from_xy(rich_z, rich_radius_min)
 rich_theta_max = theta_from_xy(rich_z, rich_radius_max)
@@ -215,9 +219,9 @@ rich_eta_max = theta_to_eta(rich_theta_max)
 rich_pt_min = momentum_cutoff(rich_radius_min/1, 1.7) #conservative estimate
 rich_pt_min2 = momentum_cutoff(rich_radius_min/2, 1.7) #not so conservative estimate
 
-display(rich_theta_min, rich_theta_max)
-display(rich_eta_min, rich_eta_max)
-display(rich_pt_min)
+print('theta:',rich_theta_min, rich_theta_max)
+print('eta:',rich_eta_min, rich_eta_max)
+print('pt min:', rich_pt_min)
 
 #time resolutions
 sigma_tof = 0.02 #ns as estimate by Alexander in email 09/24
@@ -229,76 +233,76 @@ sigma_total = np.sqrt(sigma_tof**2 + sigma_t0**2)
 # In[9]:
 
 
-xx = np.linspace(rich_eta_min, rich_eta_max, 100)
+# xx = np.linspace(rich_eta_min, rich_eta_max, 100)
 
-#lower momentum cutoff vs. eta
-plt.plot(xx, momentum_from_pt_eta(rich_pt_min, xx))
-plt.plot(xx, momentum_from_pt_eta(rich_pt_min2, xx))
-
-
-# In[10]:
+# #lower momentum cutoff vs. eta
+# plt.plot(xx, momentum_from_pt_eta(rich_pt_min, xx))
+# plt.plot(xx, momentum_from_pt_eta(rich_pt_min2, xx))
 
 
-xx = np.linspace(rich_eta_min, rich_eta_max, 100)
-
-#direct path length vs. eta
-
-plt.plot(xx, 1/np.cos(eta_to_theta(xx))*rich_z)
+# # In[10]:
 
 
-# In[11]:
+# xx = np.linspace(rich_eta_min, rich_eta_max, 100)
+
+# #direct path length vs. eta
+
+# plt.plot(xx, 1/np.cos(eta_to_theta(xx))*rich_z)
 
 
-xx = np.linspace(0.5, 10, 1000)
-
-#plot some time of flight numbers at fixed eta = -2.5 as function of momentum
-
-eta = -2.5
-
-tof_electron = vec_tof(xx, pdg_to_mass(11), 1/np.cos(eta_to_theta(eta))*rich_z*1000)
-tof_pion = vec_tof(xx, pdg_to_mass(-211), 1/np.cos(eta_to_theta(eta))*rich_z*1000)
-#tof_pion_arc = vec_tof(xx, pdg_to_mass(-211), arc_length(radius(xx, epic_B), 1/np.cos(eta_to_theta(eta))*rich_z)*1000) #this is wrong. would need to be arc length applied to transverse part of track and then added in quadrature to longitudinal part.
-tof_kaon = vec_tof(xx, pdg_to_mass(-321), 1/np.cos(eta_to_theta(eta))*rich_z*1000)
-tof_proton = vec_tof(xx, pdg_to_mass(2212), 1/np.cos(eta_to_theta(eta))*rich_z*1000)
-
-plt.plot(xx, tof_electron)
-plt.plot(xx, tof_pion)
-#plt.plot(xx, tof_pion_arc)
-plt.plot(xx, tof_kaon)
-plt.plot(xx, tof_proton)
+# # In[11]:
 
 
-# In[12]:
+# xx = np.linspace(0.5, 10, 1000)
+
+# #plot some time of flight numbers at fixed eta = -2.5 as function of momentum
+
+# eta = -2.5
+
+# tof_electron = vec_tof(xx, pdg_to_mass(11), 1/np.cos(eta_to_theta(eta))*rich_z*1000)
+# tof_pion = vec_tof(xx, pdg_to_mass(-211), 1/np.cos(eta_to_theta(eta))*rich_z*1000)
+# #tof_pion_arc = vec_tof(xx, pdg_to_mass(-211), arc_length(radius(xx, epic_B), 1/np.cos(eta_to_theta(eta))*rich_z)*1000) #this is wrong. would need to be arc length applied to transverse part of track and then added in quadrature to longitudinal part.
+# tof_kaon = vec_tof(xx, pdg_to_mass(-321), 1/np.cos(eta_to_theta(eta))*rich_z*1000)
+# tof_proton = vec_tof(xx, pdg_to_mass(2212), 1/np.cos(eta_to_theta(eta))*rich_z*1000)
+
+# plt.plot(xx, tof_electron)
+# plt.plot(xx, tof_pion)
+# #plt.plot(xx, tof_pion_arc)
+# plt.plot(xx, tof_kaon)
+# plt.plot(xx, tof_proton)
 
 
-#3 sigma separation points for same fixed eta = -2.5
-
-nsigma_pi_K = (tof_kaon - tof_pion)/sigma_total
-nsigma_K_proton = (tof_proton - tof_kaon)/sigma_total
-nsigma_e_pion = (tof_pion - tof_electron)/sigma_total
-
-plt.axhline(y = 3, color = 'black', linestyle = '--', label = '3 sigma', alpha = 0.3)
-
-plt.plot(xx, nsigma_pi_K, label = r'$\pi/K$ at $\eta$= -2.5')  
-plt.plot(xx, nsigma_K_proton, label = r'$K/p$ at $\eta$= -2.5')
-plt.plot(xx, nsigma_e_pion, label = r'$e/\pi$ at $\eta$= -2.5')
-
-plt.axvline(x = xx[np.argmax(nsigma_pi_K<3)], color = 'C0', linestyle = '--')
-plt.axvline(x = xx[np.argmax(nsigma_K_proton<3)], color = 'C1', linestyle = '--')
-plt.axvline(x = xx[np.argmax(nsigma_e_pion<3)], color = 'C2', linestyle = '--')
+# # In[12]:
 
 
+# #3 sigma separation points for same fixed eta = -2.5
 
-plt.legend()
+# nsigma_pi_K = (tof_kaon - tof_pion)/sigma_total
+# nsigma_K_proton = (tof_proton - tof_kaon)/sigma_total
+# nsigma_e_pion = (tof_pion - tof_electron)/sigma_total
 
-plt.xlabel(r'p')
-plt.ylabel(r'PID separation in units of $\sigma$')
+# plt.axhline(y = 3, color = 'black', linestyle = '--', label = '3 sigma', alpha = 0.3)
 
-plt.yscale('log')
+# plt.plot(xx, nsigma_pi_K, label = r'$\pi/K$ at $\eta$= -2.5')  
+# plt.plot(xx, nsigma_K_proton, label = r'$K/p$ at $\eta$= -2.5')
+# plt.plot(xx, nsigma_e_pion, label = r'$e/\pi$ at $\eta$= -2.5')
 
-plt.xlim(0, 10)
+# plt.axvline(x = xx[np.argmax(nsigma_pi_K<3)], color = 'C0', linestyle = '--')
+# plt.axvline(x = xx[np.argmax(nsigma_K_proton<3)], color = 'C1', linestyle = '--')
+# plt.axvline(x = xx[np.argmax(nsigma_e_pion<3)], color = 'C2', linestyle = '--')
 
-display(xx[np.argmax(nsigma_pi_K<3)], xx[np.argmax(nsigma_K_proton<3)], xx[np.argmax(nsigma_e_pion<3)])
+
+
+# plt.legend()
+
+# plt.xlabel(r'p')
+# plt.ylabel(r'PID separation in units of $\sigma$')
+
+# plt.yscale('log')
+
+# plt.xlim(0, 10)
+
+# display(xx[np.argmax(nsigma_pi_K<3)], xx[np.argmax(nsigma_K_proton<3)], xx[np.argmax(nsigma_e_pion<3)])
 
 
 # In[20]:
@@ -327,6 +331,8 @@ pid_lower_cutoff2 = momentum_from_pt_eta(rich_pt_min2, xx)
 # plt.plot(xx, pid_3sigma_cutoff_K_proton)
 # plt.plot(xx, pid_3sigma_cutoff_e_pi)
 
+plt.figure()
+
 plt.fill_between(xx, pid_lower_cutoff, pid_3sigma_cutoff_pi_K, alpha = 0.3, label = r'$\pi/K$') 
 #plt.fill_between(xx, pid_lower_cutoff, pid_3sigma_cutoff_K_proton, alpha = 0.3, label = r'$K/p$')
 #plt.fill_between(xx, pid_lower_cutoff, pid_3sigma_cutoff_e_pi, alpha = 0.3, label = r'$e/\pi$')
@@ -340,6 +346,7 @@ plt.ylabel(r'3sigma cutoff p [GeV/c]')
 # In[15]:
 
 
+plt.figure()
 plt.fill_between(xx, pid_lower_cutoff, pid_3sigma_cutoff_pi_K, alpha = 0.3, label = r'$\pi/K$') 
 
 plt.yscale('log')
@@ -353,7 +360,7 @@ plt.savefig('pfrich_contour_lin_pi_K.svg')
 
 # In[16]:
 
-
+plt.figure()
 plt.fill_between(xx, pid_lower_cutoff, pid_3sigma_cutoff_K_proton, alpha = 0.3, label = r'$\pi/K$') 
 
 plt.yscale('log')
@@ -367,7 +374,7 @@ plt.savefig('pfrich_contour_lin_K_proton.svg')
 
 # In[17]:
 
-
+plt.figure()
 plt.fill_between(xx, pid_lower_cutoff, pid_3sigma_cutoff_e_pi, alpha = 0.3, label = r'$\pi/K$') 
 
 plt.yscale('log')
